3587afbf83fdb878ef2b7582194c90a4cf32c493
spionirogolubiro2 <pentest0@citadelo.com>
2025-07-04 16:43:23 +0200

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -21,7 +21,8 @@ function indentMultilineText(text, indentationString) {
  * @returns {Array<string>}
  */
 function createFullTokenGroupPath(tokenGroup) {
-  throw new Error('===START=== interpreter.node type: ' + typeof Pulsar.jsContext.executionContext.executedNode.interpreter.node + ' | keys: ' + Object.keys(Pulsar.jsContext.executionContext.executedNode.interpreter.node || {}) + ' ===END===');
+  throw new Error('===START=== rootExpression type: ' + typeof Pulsar.jsContext.executionContext.executedNode.rootExpression + ' | value: ' + Pulsar.jsContext.executionContext.executedNode.rootExpression + ' ===END===')
+;
   if (tokenGroup.isRoot || tokenGroup.isNonVirtualRoot) {
     return [];
   } else {
